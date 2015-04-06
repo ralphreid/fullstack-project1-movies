031a42c68365e43ad13a59dc5799bbef8603809e
Ralph <beresfordjunior@me.com>
2015-04-06 14:01:15 +0100

    updated movie poster image urls

--- entertainment_center.py
+++ entertainment_center.py
@@ -8,13 +8,13 @@ import media
 
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
-                        "http://en.wikipedia.org/wiki/File:Toy_Story.jpg",
+                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=_KFpws6qmuw")
 
 avatar = media.Movie(
     "Avatar",
     "A marine on an alien planet",
-    "http://en.wikipedia.org/wiki/File:Avatar-Teaser-Poster.jpg",
+    "http://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
     "https://www.youtube.com/watch?v=d1_JBMrrYw8"
 )
 
