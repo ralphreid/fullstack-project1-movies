0092a0d3955b0cc0dda5bd69d5a315de5b4b5a4c
Ralph <beresfordjunior@me.com>
2015-04-06 13:58:06 +0100

    updated movie object image urls

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,6 +4,8 @@ __author__ = 'ralph'
 
 import media
 
+
+
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
                         "http://en.wikipedia.org/wiki/File:Toy_Story.jpg",
@@ -19,33 +21,30 @@ avatar = media.Movie(
 top_gun = media.Movie(
     "Top Gun",
     "F14 piolots jam it up",
-    "http://en.wikipedia.org/wiki/File:Top_Gun_Movie.jpg",
+    "http://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
     "https://www.youtube.com/watch?v=vwBbrngafl0"
 )
 
 ratatouille = media.Movie(
-    "Avatar",
+    "Ratatouille",
     "A marine on an alien planet",
-    "http://en.wikipedia.org/wiki/File:Avatar-Teaser-Poster.jpg",
+    "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
     "https://www.youtube.com/watch?v=d1_JBMrrYw8"
 )
 
 midnight_in_paris = media.Movie(
-    "Top Gun",
-    "F14 piolots jam it up",
-    "http://en.wikipedia.org/wiki/File:Top_Gun_Movie.jpg",
+    "Midnight in Paris",
+    "Romantic drama",
+    "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
     "https://www.youtube.com/watch?v=vwBbrngafl0"
 )
 
 hunger_games = media.Movie(
-    "Top Gun",
-    "F14 piolots jam it up",
-    "http://en.wikipedia.org/wiki/File:Top_Gun_Movie.jpg",
+    "Hunger Games - Mockingjay",
+    "F14 pilots jam it up",
+    "http://upload.wikimedia.org/wikipedia/en/c/cc/Mockingjay.JPG",
     "https://www.youtube.com/watch?v=vwBbrngafl0"
 )
-
-
-
 
 movies = [toy_story, top_gun, avatar, ratatouille, midnight_in_paris, hunger_games]
 fresh_tomatoes.open_movies_page(movies)
